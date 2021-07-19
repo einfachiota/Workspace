adaf6073a9b410e916c4d35c87581ffc1e7ad750
Cryptec <neupi92@web.de>
2021-07-19 13:44:59 +0200

    added icons to sidebar

--- Frontend/src/components/sidebaritems.jsx
+++ Frontend/src/components/sidebaritems.jsx
@@ -1,11 +1,11 @@
-//import { GrAdd, BsCollection } from "../assets/icons";
+import { GithubIcon, AiOutlineDashboard } from "../assets/icons";
 
 export const SidebarItemsMain = [
 
     {
         name: "Dashboard",
         route: '/',
-        //icon: <GrAdd />
+        icon: <AiOutlineDashboard />
     },
 ];
 
@@ -13,7 +13,7 @@ export const SidebarItemsResources = [
     {
         name: "GitHub",
         href: '/github',
-        //icon: <GrAdd />
+        icon: <GithubIcon />
     },
 ]
 export default SidebarItemsMain;
