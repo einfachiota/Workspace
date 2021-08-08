d28e9089eb22584d1f1ecb810d599708c27c7cfb
Cryptec <neupi92@web.de>
2021-08-08 22:20:47 +0200

    clean code

--- Frontend/src/Screens/Meetings.js
+++ Frontend/src/Screens/Meetings.js
@@ -7,14 +7,6 @@ import { showMd, hideMd } from '../components/handler'
 
 import '../css/meetings.css'
 
-const AppContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
 const Title = styled.div`
   font-size: 25px;
   font-weight: 700;
