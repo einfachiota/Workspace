6d2b0ae51443145cfab488546552f469c1e0c855
Jonathan Fritz <fritz@nosc.io>
2021-08-09 18:23:14 +0200

    Revamping user registration and login system

---
+++ Backend/auth/validate_auth.js
@@ -0,0 +1,3 @@
+export function validate_auth(res, req, next) {
+	next()
+}
--- Backend/index.js
+++ Backend/index.js
@@ -2,29 +2,79 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
-
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
-
-// Import Routes
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
 const authRoute = require('./routes/auth');
 const analyticRoute = require('./routes/analytic');
 const meetingRoute = require('./routes/meetings')
+const db = require('./Database');
+const argon2 = require("argon2")
+require('dotenv').config()
 
-// Middleware
-app.use(express.json());
+/**
+ * Sets up passport (authentication)
+ */
+function setupPassport() {
+  console.log("Setting up passport")
+  passport.use(new LocalStrategy(
+    async function(username, password, done) {
+      db.get(`SELECT * FROM Employees WHERE email = ?`,[username], async (err, user) => {
+        if (password)
+        {
+          console.log("testfor " + password)
+          const res = await argon2.verify(user.password, password + process.env.SALT)
+          if (res)
+            done(err, user)
+          else 
+            done(err, null)
+        }
+        else
+          done(err, null)
+      })
+    }
+  ));
+  
+  passport.serializeUser((user, done) => {
+    done(null, user.email);
+  });
 
-// Route Middlewares
-app.use('/api', authRoute);
-app.use('/api', analyticRoute);
-app.use('/api', meetingRoute);
+  passport.deserializeUser((id, done) => {
+    db.get(`SELECT * FROM Employees WHERE email = ?`,[username], (err, user) => {
+      console.log(user)
+      if (user)
+        done(err, user)
+      else
+        done(err, null);
+    });
+  });
+}
 
 
-var HTTP_PORT = 5000
-// Start server
-app.listen(HTTP_PORT, () => {
-  console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
-});
+/**
+ * Sets up express
+ */
+function setupExpress() {
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(cors());
+  app.use(express.json());
+  app.use(passport.initialize());
+  app.use(passport.session());  
+  setupPassport()
+
+  // Route Middlewares
+  app.use('/api', authRoute);
+  app.use('/api', analyticRoute);
+  app.use('/api', meetingRoute);
+
+
+  var HTTP_PORT = 5000
+  // Start server
+  app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
+  }); 
+}
+
+setupExpress();
