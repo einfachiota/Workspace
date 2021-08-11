aabc97d83adb803a6db8dd52b9059c7ff92f7854
Jonathan Fritz <fritz@nosc.io>
2021-08-11 10:22:02 +0200

    Now creating initial admin user with provided env values

--- Backend/Database.js
+++ Backend/Database.js
@@ -1,8 +1,9 @@
 var sqlite3 = require('sqlite3').verbose()
 const bcrypt = require('bcryptjs')
-
+const register_user = require("./database/register_user")
 
 const DBSOURCE = "db.sqlite"
+require('dotenv').config()
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
@@ -23,15 +24,14 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 if (err) {
                     // Table already created
                 } else {
-                    // Hashing
-                    var salt = bcrypt.genSaltSync(10);
-                    var hashedPassword = bcrypt.hashSync("admin123456", salt);
-                    // Table just created, creating some rows
-                    var insertUsers = 'INSERT INTO Employees (name, email, password) VALUES (?,?,?)'
-                    db.run(insertUsers, ["admin","admin@example.com", hashedPassword])
-            
+                    // First start. Create initial admin user now!
+                    if (process.env.INITIAL_ADMIN_PASSWORD && process.env.ADMIN_EMAIL)
+                    {
+                        register_user("admin", process.env.ADMIN_EMAIL, process.env.INITIAL_ADMIN_PASSWORD )
+                        console.log("Created inital user!")
                     }
-                });
+                    else
+                        console.log("[ERROR] Failed to create initial admin user!")
             }
             });
 
